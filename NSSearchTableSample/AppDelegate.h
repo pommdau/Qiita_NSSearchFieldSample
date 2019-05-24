ffac3a2af2f3b2e5476a7c7b862fabcbadbb7b04
hikeuchi <hikeuchi@morisawa.co.jp>
2019-05-24 14:35:53 +0900

    initial commit

---
+++ NSSearchTableSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSSearchTableSample
+//
+//  Created by HIROKI IKEUCHI on 2019/05/24.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSSearchTableSample/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  NSSearchTableSample
+//
+//  Created by HIROKI IKEUCHI on 2019/05/24.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
